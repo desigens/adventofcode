b475196f7189eeafaf00567903187ad9dc2bd0b9
Denis Denisov <desigens@gmail.com>
2017-12-07 23:26:06 +0300

    Day 6, part 1

---
+++ days/6/6-1.js
@@ -0,0 +1,62 @@
+// const arr = [0, 2, 7, 0];
+const arr = [10,3,15,10,5,15,5,15,9,2,5,8,5,2,3,6];
+
+// Step counter zero
+let counter = 0;
+
+const hash = {};
+
+// While hash not contains state
+while (!hash[arr.join(' ')]) {
+
+	// Set hash key true
+	hash[arr.join(' ')] = true;
+
+	// Increase step counter
+	counter++;
+
+	// Do Routine
+	routine()
+}
+
+// If contains return step counter
+console.log(counter);
+
+// Routine
+function routine() {
+
+	// Find max index
+	let index = getIndexOfMax(arr);
+	
+	// Get index value to tmp
+	let tmp = arr[index];
+	
+	// Set index value to zero
+	arr[index] = 0;
+
+	// While tmp > zero
+	while (tmp > 0) {
+
+		// Get next index
+		// If next index equal length next index is zero
+		index = index === arr.length - 1 ? 0 : index + 1;
+
+		// Increase next index value
+		arr[index] = arr[index] + 1;
+
+		// Decrease tmp
+		tmp = tmp - 1;
+	}
+}
+
+function getIndexOfMax(arr) {
+	let i = 0;
+	let max = 0;
+	arr.forEach((a, index) => {
+		if (a > max) {
+			max = a;
+			i = index;
+		}
+	})
+	return i;
+}
